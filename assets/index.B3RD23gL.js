ba6fa486b67c99e7b102055cb62962de4ef4b573
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-02-29 18:10:22 +0100

    Add files via upload

---
+++ assets/index.B3RD23gL.js
@@ -0,0 +1,1 @@
+import{B as t,o as n,c as s,a as e,t as o,F as r}from"./entry.DwSIqJ-S.js";const l=e("h1",null,"Index Page",-1),u={__name:"index",setup(c){return t(),(a,_)=>(n(),s(r,null,[l,e("p",null,o(a.data),1)],64))}};export{u as default};
